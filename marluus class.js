87e9f7e000a3e67dc1b2c0136b0dfbb1d4d9364f
lcofili <lcofili2000@gmail.com>
2021-05-14 14:09:26 +0100

    learnt logical operator

---
+++ marluus class.js
@@ -0,0 +1,46 @@
+//Logical Operator
+
+console.log(3>4 && 4>3)
+console.log(7===7|| 5<3)
+
+let a = 5
+let b = 10
+let c = 5
+console.log(a==5 && a===c && b==(a*2))
+
+let e = 15
+let f = "15"
+console.log(!(e=== f))
+console.log(!(e==f))
+
+
+// // FUNCTIONs 
+unction sum () {
+let a = 10
+let b = 15
+console.log(a+b)
+return result = a+b
+
+}
+console.log(sum ())
+
+const add = () => {
+
+let a = 10
+let b = 15
+return a + b
+}
+console.log(add())
+
+function add (a, b) {
+
+return a+b
+}
+console.log(add(7, 8))
+
+function div (c, d) {
+
+    return c/d
+}
+console.log(div(10, 5))
+
